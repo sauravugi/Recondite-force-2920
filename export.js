fc42eff9da002374e391446c8ce5326182a97dcc
Suresh17 <suresh.20fk@gmail.com>
2022-07-23 19:43:11 +0530

    added merged navbar

---
+++ export.js
@@ -0,0 +1,38 @@
+let submitFunc = ()=>{
+    return `<body>
+<div id="cont">
+    <div id="cards">
+        <div id="user">
+            <h1>Hi Suresh 👋</h1>
+        </div>
+        <div>Thanks for contributing to Unsplash! You’re awesome. To save you time and increase your chance of being featured, please ensure that your photos meet our <a href="https://help.unsplash.com/en/articles/2534415-submission-guidelines">submission guidelines.</a></div>
+        <div>Note: Every photo on Unsplash is published under the <a href="https://unsplash.com/license">Unsplash License</a> – which allows people to use photos from Unsplash for free, including for commercial purposes, without attributing the photographer on Unsplash.<a href="https://unsplash.com/license">Learn more.</a></div>
+    </div>
+    <div id="image">
+        <img src="https://cms-assets.tutsplus.com/cdn-cgi/image/width=850/uploads/users/2659/posts/27721/image-upload/Minimalist%20Polaroid%20Collage%20Template.jpg" alt="">
+    </div>
+</div>
+<div id="footer">
+    <div id="first">
+        <img src="https://i.pinimg.com/736x/28/03/4e/28034e601b054650d3a70e382db80b44.jpg" style="width: 90px; height: 90px;" alt="">
+        <p>Only upload high quality photos. For a standard landscape orientation photo, 5 megapixels is at least 2,500 by 2,000 pixels in size.</p>
+    </div>
+    <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTH0nFwWeJYHlJI25kEm_6qZjS35S2KuXfUSv-3nJR-wxupfQhmuVsbZx6wB8GPxAQBFtI&usqp=CAU" alt="">
+        <p>Photos are clear, original and are not over edited. Photos do not contain nudity or violent imagery. Unsplash is a place for people of all ages.</p>
+    </div>
+    <div>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRE359fwjE5i05tplpr13Xwz8VDltWyUxuFdA&usqp=CAU" alt="">
+        <p>You must own the rights to any photo you share on Unsplash. You can read more about this in our <a href="">Terms.</a></p>
+    </div>
+</div>
+
+<div id="bottom">
+    <input type="checkbox">
+    <label for="">I understand and agree</label>
+    <input type="file">
+</div>
+</body>`
+}
+
+export default submitFunc;
---
+++ submitLI.js
@@ -0,0 +1,3 @@
+import submitFunc from "./export.js";
+
+document.getElementById("main_body").innerHTML = submitFunc();
